a5bf1199fe2315cd334486abdaac3d35c3711478
장혜림 <51152660+hxerimione@users.noreply.github.com>
2024-06-30 11:00:06 +0900

    Title: 생명과학부 랩 인턴, Time: 146ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240630/생명과학부 랩 인턴/biology-lab-intern.py
+++ 240630/생명과학부 랩 인턴/biology-lab-intern.py
@@ -11,6 +11,7 @@ for _ in range(k):
     x,y,s,d,b = map(int,input().split())
     graph[x-1][y-1]= [s,d,b]
 
+#방향 변경
 def changeD(n):
     if n==1:
         return 2
@@ -20,6 +21,35 @@ def changeD(n):
         return 4
     if n==4:
         return 3
+def move(a,b,cur_s,cur_d):
+    
+    #위아래 - n,a
+    if cur_d in [1,2]:
+        if cur_s> 2*n-2 :
+            cur_s = cur_s % (2*n-2)
+    #양옆 - m,b
+    if cur_d in [3,4]:
+        if cur_s> 2*m-2 :
+            cur_s = cur_s % (2*m-2)
+    da = a + cur_s*dx[cur_d]
+    db = b + cur_s*dy[cur_d]
+    cnt=-1
+    while True:
+        cnt += 1
+        if da<0 :
+            da = -da
+        elif da>=n:
+            da = 2*n-da-2
+        elif db<0:
+            db = -db
+        elif db>=m :
+            db = 2*m-db-2
+        else:
+            if cnt%2==1 :
+                cur_d = changeD(cur_d)
+            break
+    return da,db,cur_d
+
 for i in range(m):
     #1 - 열 탐색
     for j in range(n):
@@ -32,40 +62,20 @@ for i in range(m):
         for b in range(m):
             if graph[a][b] !=[]:
                 cur_s,cur_d,cur_b = graph[a][b]
-                # print("cur",cur_s,cur_d,cur_b)
-                #곰팡이 발견
-                da = a + cur_s*dx[cur_d]
-                db = b + cur_s*dy[cur_d]
-                #벽과 충돌
-                # print("da",da,"db",db)
-                cnt=-1
-                while True:
-                    cnt += 1
-                    if da<0 :
-                        da = -da
-                    elif da>=n:
-                        da = 2*n-da-2
-                    elif db<0:
-                        db = -db
-                    elif db>=m :
-                        db = 2*m-db-2
-                    else:
-                        if cnt%2==1 :
-                            cur_d = changeD(cur_d)
-                        break
-                # print("changed","da",da,"db",db,"cur_d",cur_d)
+
+                da,db,cur_d = move(a,b,cur_s,cur_d)                
+
                 #이미 곰팡이 있을 시
                 if update_graph[da][db] != []:
-                    # print("마주침")
                     if update_graph[da][db][2] > cur_b :
                         break
                 #이동
                 update_graph[da][db] = [cur_s,cur_d,cur_b]
+
+    #3 초기화
     for a in range(n):
         for b in range(m):
             graph[a][b] = update_graph[a][b]
     update_graph = [[[]]*m for _ in range(n)]
     
-    # print(graph)
-    # print()
 print(answer)
