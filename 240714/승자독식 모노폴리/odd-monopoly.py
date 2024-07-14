5eda7b189d2ae1979051330feffa87bb51fbc544
장혜림 <51152660+hxerimione@users.noreply.github.com>
2024-07-14 11:26:02 +0900

    Title: 승자독식 모노폴리, Time: 231ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 240714/승자독식 모노폴리/odd-monopoly.py
@@ -0,0 +1,85 @@
+import sys
+from collections import deque,defaultdict
+input = sys.stdin.readline
+dx,dy = [0,-1,1,0,0],[0,0,0,-1,1]
+n,m,k = map(int,input().split())
+
+graph = [[] for _ in range(n)] #계약
+move_graph = [[] for _ in range(n)] #위치
+
+for i in range(n):
+    arr = list(map(int,input().split()))
+    for a in arr:
+        move_graph[i].append([a])
+        if a==0:
+            graph[i].append([0,0])
+        else:
+            graph[i].append([a,k])
+#플레이어 방향
+direction = [0] + list(map(int,input().split()))
+
+live = [0] + [1]*m
+
+#방향 우선순위
+priority_d = defaultdict(list)
+for i in range(1,m+1):
+    priority_d[i].append([0])
+    for _ in range(4):
+        priority_d[i].append(list(map(int,input().split())))
+
+for T in range(1,1000):
+    #독점 계약기간 줄이기
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j][1] >1:
+                graph[i][j][1]-=1
+            elif graph[i][j][1] ==1:
+                graph[i][j] = [0,0]
+    
+    #이동
+    for i in range(n):
+        for j in range(n):
+            if move_graph[i][j][0] >0:
+                p = move_graph[i][j][0]
+                d_now = direction[p]
+            else:
+                continue
+            next_x,next_y,next_d= -1,-1,-1
+            for d in priority_d[p][d_now]:
+                #아무도 독점계약 안한 칸 찾기
+                nx = i+ dx[d]
+                ny = j+ dy[d]
+                if 0<=nx<n and 0<=ny<n and graph[nx][ny][0] ==0 :
+                    next_x,next_y,next_d = nx,ny,d
+                    break
+            else:
+                #내가 독점계약 한 찾기
+                for d in priority_d[p][d_now]:
+                    nx = i+ dx[d]
+                    ny = j+ dy[d]
+                    if 0<=nx<n and 0<=ny<n and graph[nx][ny][0] ==p :
+                        next_x,next_y,next_d = nx,ny,d
+                        break
+            # 이동
+            move_graph[next_x][next_y].append(p)
+            move_graph[i][j] = [0]
+            # 계약 안된 칸이면 계약하기
+            if graph[next_x][next_y][0] == 0:
+                graph[next_x][next_y]=[p,k]
+    #겹치는 경우 제일 작은 플레이어만 남기기
+    for i in range(n):
+        for j in range(n):
+            if len(move_graph[i][j])>1:
+                if 0 in move_graph[i][j]:
+                    move_graph[i][j].remove(0)
+                min_p = min(move_graph[i][j])
+                for pi in move_graph[i][j] :
+                    if pi != min_p:
+                        live[pi] = 0
+                move_graph[i][j] = [min_p]
+    if sum(live) == 1:
+        print(T)
+        break
+    
+else:
+    print(-1)
